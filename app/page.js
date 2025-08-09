ec5f384fb9e424983303e5a08d09f3773ee7c4eb
Kabil Nages <kabiln@gmail.com>
2025-08-09 14:51:01 +0200

    small formatting changes

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ import animationData from "../public/ficufish.json";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans grid items-center justify-items-center min-h-screen">
+      <main className="flex flex-col gap-2 row-start-2 items-center sm:items-center-safe">
         <div className="flex flex-row row-auto items-center sm:items-center-safe">
           <Lottie
           className="w-32"
@@ -19,7 +19,7 @@ export default function Home() {
             ficufish
           </h1>
         </div>
-        <ol className="font-mono list-inside list-none text-sm/6 text-center sm:text-left w-xl">
+        <ol className="font-mono list-inside list-none text-sm/6 text-center sm:text-center max-w-2xl">
           <li className="mb-2 tracking-[-.01em]">
             We research and develop innovative solutions empowering people and organizations worldwide to create thriving circular economies.
           </li>
