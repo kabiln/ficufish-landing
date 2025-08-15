b747ee35715f35de43364bfe95d84d24e6fd6af0
Kabil Nages <kabiln@gmail.com>
2025-08-15 12:52:53 +0200

    form finally working

--- app/contact.js
+++ app/contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
   export default function Contact() {
+    const [submissionStatus, setSubmissionStatus] = useState(null);
     async function handleSubmit(e) {
         e.preventDefault();
         const response = await fetch("https://api.web3forms.com/submit", {
@@ -10,16 +11,18 @@ import React, { useState } from 'react';
                 Accept: "application/json",
             },
             body: JSON.stringify({
-                access_key: "YOUR_ACCESS_KEY_HERE",
+                access_key: "39cefe42-ef46-4bd9-9354-6a6724a2b325",
                 name: e.target.name.value,
                 email: e.target.email.value,
                 message: e.target.message.value,
             }),
         });
         const result = await response.json();
-        if (result.success) {
-            console.log(result);
-        }
+    if (result.success) {
+      setSubmissionStatus("success");
+    } else {
+      setSubmissionStatus("error");
+    }
     }
 
   return (
@@ -38,6 +41,12 @@ import React, { useState } from 'react';
           </div>
           <button type="submit" className="w-full bg-slate-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-white hover:text-slate-700 focus:outline-none focus:ring-4 focus:ring-white-300 transition-all duration-300 shadow-md hover:shadow-lg">Submit Form</button>
       </form>
+      {submissionStatus === "success" && (
+        <p className="success-message">Thank you for submitting the form! We'll get back to you soon.</p>
+      )}
+      {submissionStatus === "error" && (
+        <p className="error-message">Error submitting the form. Please try again.</p>
+      )}
      </div>
     </div>
   );
